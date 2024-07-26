40ab9c539a95869410f2cec2628a773c960e967c
Belayneh <bela@localhost.localdomain>
2024-07-26 20:47:10 +0300

    update about and tour package pages

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        margin: {
+          '5p': '5%',   // 5% margin
+          '10p': '10%', // 10% margin
+          '15p': '15%', // 15% margin
+          '20p': '20%', // 20% margin
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
+  
